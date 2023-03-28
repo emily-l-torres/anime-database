3f4bcffcdf0a55f27048f12f5aafaba439ee5a8e
Emily Torres <106714585+emily-l-torres@users.noreply.github.com>
2023-03-28 12:23:44 -0400

    Add files via upload

---
+++ scheme_anime_database.scm
@@ -0,0 +1,539 @@
+(define anime-database
+  (list
+   (make-anime
+    '(angel beats)
+    '(enjeru biitsu)
+    2010
+    13
+    '((action)
+        (comedy)
+        (drama)
+        (school)
+        (supernatural))
+     '(jun maeda))
+   (make-anime
+    '(assassination classroom)
+    '(ansatsy kyoushitsu)
+    2015
+    47
+    '((action)
+        (comedy)
+        (school)
+        (shounen))
+     '(yusei matsui))
+   (make-anime
+    '(astro boy)
+    '(tetsuwan atomu)
+    1963
+    193
+    '((action)
+        (sci-fi)
+        (adventure)
+        (drama)
+        (shounen))
+    '(osamu tezuka))
+   (make-anime
+    '(attack on titan)
+    '(shingeki no kyojin)
+    2013
+    75
+    '((action)
+        (military)
+        (mystery)
+        (drama)
+        (shounen))
+    '(hajime isayama))
+   (make-anime
+    '(azumanga daioh)
+    '(azumanga daioh)
+    2002
+    26
+    '((slice of life)
+        (comedy)
+        (school))
+    '(kiyohiko azuma))
+   (make-anime
+    '(backstreet girls gokudolls)
+    '(back street girls: washira idol hajimemashita)
+    2018
+    10
+    '((comedy)
+      (ecchi)
+      (seinen))
+    '(jasmine gyuh))
+   (make-anime
+    '(black butler)
+    '(kuroshitsuji)
+    2008
+    24
+    '((action)
+        (mystery)
+        (comedy)
+        (historical)
+        (shounen))
+    '(yana toboso))
+   (make-anime
+    '(bleach)
+    '(burichi)
+    2004
+    366
+    '((action)
+        (adventure)
+        (comedy)
+        (supernatural)
+        (shounen))
+    '(tite kubo))
+   (make-anime
+    '(cells at work)
+    '(hataraku saibo)
+    2018
+    21
+    '((comedy)
+       (shounen))
+    '(akane shimuzu))
+   (make-anime
+    '(chobits)
+    '(chobittsu)
+    2002
+    27
+    '((sci-fi)
+       (comedy)
+       (drama)
+       (romance)
+       (shounen))
+    '(clamp))
+   (make-anime
+    '(citrus)
+    '(shitorasu)
+    2018
+    12
+    '((drama)
+       (romance)
+       (school)
+       (shoujo ai))
+    '(clamp))
+    (make-anime
+     '(clannad)
+     '(kuranado)
+     2007
+     47
+     '((comedy)
+       (drama)
+       (romance)
+       (school)
+       (slice of life))
+     '(key))
+    (make-anime
+     '(cowboy bebop)
+     '(kauboi bibappu)
+     1998
+     26
+     '((action)
+       (adventure)
+       (comedy)
+       (drama)
+       (sci-fi))
+     '(hajime yatate))
+    (make-anime
+     '(death note)
+     '(desunoto)
+     2006
+     37
+     '((mystery)
+       (police)
+       (psychological)
+       (thriller)
+       (shounen))
+     '((tsugumi ohba)
+       (takeshi obata)))
+    (make-anime
+     '(dororo)
+     '(dororo)
+     1969
+     26
+     '((action)
+       (adventure)
+       (historical)
+       (samurai)
+       (shounen))
+     '(osamu tezuka))
+    (make-anime
+     '(dragonball)
+     '(doragon boru)
+     1986
+     153
+     '((adventure)
+       (comedy)
+       (fantasy)
+       (martial arts)
+       (shounen))
+     '(akira toriyama))
+    (make-anime
+     '(dragonball z)
+     '(doragon boru zetto)
+     1989
+     291
+     '((adventure)
+       (comedy)
+       (fantasy)
+       (martial arts)
+       (shounen))
+     '(akira toriyama))
+    (make-anime
+     '(food wars)
+     '(shokugeki no soma)
+     2015
+     86
+     '((ecchi)
+       (school)
+       (shounen))
+     '((yuto tsukuda)
+       (shun saeki)))
+    (make-anime
+     '(fruits basket)
+     '(furutsu basuketto)
+     2001
+     53
+     '((slice of life)
+       (comedy)
+       (supernatural)
+       (drama)
+       (shoujo))
+     '(natsuki takaya))
+    (make-anime
+     '(fullmetal alchemist)
+     '(hagane no renkinjutsushi)
+     2003
+     51
+     '((action)
+       (adventure)
+       (comedy)
+       (military)
+       (shounen))
+     '(hiromu arakawa))
+    (make-anime
+     '(silver soul)
+     '(gin tama)
+     2006
+     201
+     '((action)
+       (comedy)
+       (historical)
+       (samurai)
+       (shounen))
+     '(hideaki sorachi))
+    (make-anime
+     '(hellsing)
+     '(herushingu)
+     2001
+     13
+     '((action)
+       (horror)
+       (supernatural)
+       (vampire)
+       (seinen))
+     '(kouta hirano))
+    (make-anime
+     '(hunter x hunter)
+     '(hanta x hanta)
+     1999
+     62
+     '((action)
+       (adventure)
+       (super power)
+       (fantasy)
+       (shounen))
+     '(yoshihiro togashi))
+    (make-anime
+     '(i want to eat your pancreas)
+     '(kimi no suizou wo tabetai)
+     2018
+     1
+     '((slice of life)
+       (drama)
+       (romance))
+     '(yoru sumino))
+    (make-anime
+     '(dog yaksha)
+     '(inuyasha)
+     2000
+     167
+     '((action)
+       (adventure)
+       (comedy)
+       (magic)
+       (shounen))
+     '(rumiko takahashi))
+    (make-anime
+     '(jojo's bizarre adventure)
+     '(jojo no kimyou na bouken)
+     2012
+     152
+     '((action)
+       (adventure)
+       (supernatural)
+       (vampire)
+       (shounen))
+     '(hirohiko araki))
+    (make-anime
+     '(the troubled life of miss kotoura)
+     '(kotoura-san)
+     2013
+     12
+     '((comedy)
+       (drama)
+       (romance)
+       (school))
+     '(enokizu))
+    (make-anime
+     '(lucky star)
+     '(raki suta)
+     2007
+     24
+     '((slice of life)
+       (comedy)
+       (parody)
+       (school))
+     '(kagami yoshimizu))
+    (make-anime
+     '(maid sama)
+     '(kaichohameidosama)
+     2010
+     26
+     '((comedy)
+       (romance)
+       (school)
+       (shoujo))
+     '(hiro fujiwara))
+    (make-anime
+     '(my ordinary life)
+     '(nichijou)
+     2011
+     26
+     '((slice of life)
+       (comedy)
+       (school)
+       (shounen))
+     '(keiichi arawi))
+    (make-anime
+     '(naruto)
+     '(naruto)
+     2002
+     500
+     '((action)
+       (adventure)
+       (comedy)
+       (martial arts)
+       (shounen))
+     '(masashi kishimoto))
+    (make-anime
+     '(negima: magister negi magi)
+     '(maho sensei negima)
+     2005
+     26
+     '((comedy)
+       (ecchi)
+       (fantasy)
+       (magic)
+       (shounen))
+     '(ken akamatsu))
+    (make-anime
+     '(one piece)
+     '(hitotsunagi)
+     1999
+     970
+     '((action)
+       (adventure)
+       (comedy)
+       (drama)
+       (shounen))
+     '(eiichiro oda))
+    (make-anime
+     '(one punch man)
+     '(wanpanman)
+     2015
+     24
+     '((action)
+       (sci-fi)
+       (comedy)
+       (parody)
+       (supernatural))
+     '(ONE))
+    (make-anime
+     '(ouran high school host club)
+     '(ouran koukou hosutobu)
+     2006
+     26
+     '((comedy)
+       (harem)
+       (romance)
+       (school)
+       (shoujo))
+     '(bisco hitari))
+    (make-anime
+     '(place to place)
+     '(acchi kocchi)
+     2012
+     12
+     '((slice of life)
+       (comedy)
+       (romance)
+       (school))
+     '(ishiki))
+    (make-anime
+     '(potemayo)
+     '(potemayo)
+     2007
+     12
+     '((comedy)
+       (school)
+       (seinen))
+     '(haruka ogataya))
+    (make-anime
+     '(ranma 1/2)
+     '(ranma nibun-no-ichi)
+     1989
+     18
+     '((action)
+       (comedy)
+       (ecchi)
+       (martial arts)
+       (shounen))
+     '(rumiko takahashi))
+    (make-anime
+     '(rascal does not dream of bunny girl senpai)
+     '(seishun buta yaro)
+     2018
+     13
+     '((comedy)
+       (supernatural)
+       (drama)
+       (romance)
+       (shounen))
+     '(keeji mizoguchi))
+    (make-anime
+     '(rurouni kenshin)
+     '(ruronikenshin -meji kenkaku romantan-)
+     1996
+     94
+     '((action)
+       (adventure)
+       (comedy)
+       (romance)
+       (samurai))
+     '(nobuhiro watsuki))
+    (make-anime
+     '(sailor moon)
+     '(bishojo senshi sera mun)
+     1992
+     200
+     '((demons)
+       (magic)
+       (romance)
+       (shoujo))
+     '(naoko takeuchi))
+    (make-anime
+     '(school rumble)
+     '(sukururanburu)
+     2004
+     26
+     '((comedy)
+       (romance)
+       (school)
+       (shounen))
+     '(jin kobayashi))
+    (make-anime
+     '(soul eater)
+     '(soru ita)
+     2008
+     51
+     '((action)
+       (fantasy)
+       (comedy)
+       (supernatural)
+       (shounen))
+     '(atsushi okubo))
+    (make-anime
+     '(the devil is a part-timer)
+     '(hataraku mao-sama)
+     2013
+     13
+     '((comedy)
+       (fantasy)
+       (romance)
+       (supernatural))
+     '((satoshi wagahara)
+       (akio hiiragi)))
+    (make-anime
+     '(to love ru)
+     '(toraburu)
+     2008
+     26
+     '((sci-fi)
+       (harem)
+       (comedy)
+       (romance)
+       (shounen))
+     '((saki hasemi)
+       (kentaro yabuki)))
+    (make-anime
+     '(toradora)
+     '(toradora)
+     2008
+     25
+     '((slice of life)
+       (comedy)
+       (romance)
+       (school))
+     '((yuyuko takemiya)
+       (zekkyo)))
+    (make-anime
+     '(vampire knight)
+     '(banpaia naito)
+     2008
+     13
+     '((drama)
+       (mystery)
+       (romance)
+       (shoujo)
+       (supernatural))
+     '(matsuri hino))
+    (make-anime
+     '(world trigger)
+     '(warudo toriga)
+     2014
+     85
+     '((action)
+       (sci-fi)
+       (supernatural)
+       (school)
+       (shounen))
+     '(daisuke ashihara))
+    (make-anime
+     '(your lie in april)
+     '(shigatsu wa kimi no uso)
+     2014
+     22
+     '((drama)
+       (music)
+       (romance)
+       (school)
+       (shounen))
+     '(naoshi arakawa))
+    (make-anime
+     '(yuri on ice)
+     '(yuri on ice)
+     2016
+     12
+     '((comedy)
+       (sports))
+     '(misturo kubo))))
+    
+    
+    
+   
+   
+   
+     
+     
+   
